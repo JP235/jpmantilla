6528d7742cc290fdccee966600e372dac5036b70
jpmanti <jpmanti@gmail.com>
2022-06-22 14:10:50 -0500

    cleanup

--- src/pages/ListGames/ListGames.js
+++ src/pages/ListGames/ListGames.js
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import { selectBoards, selectIsLoading } from "../../api/boards/boardsAPISlice";
+import { selectBoards } from "../../api/boards/boardsAPISlice";
 import { getBoards } from "../../api/boards/boardsAPI";
 import { BoardPics } from "../../common/canvas/BoardPics";
 
 function ListGames() {
 	const boards = useSelector(selectBoards);
-	const isLoading = useSelector(selectIsLoading);
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
 	useEffect(() => {
 		dispatch(getBoards());
@@ -16,18 +15,12 @@ function ListGames() {
 
 	return (
 		<ul>
-			{isLoading === true ? (
-				<div> "Loading..."</div>
-			) : (
-				boards.map((board) => (
-
-						<li key={board.code}  style={{ listStyleType: "none" }}>
-							<BoardPics board={board} />
-							{"   "}
-						</li>
-						
-				))
-			)}
+			{boards.map((board) => (
+				<li key={board.code} style={{ listStyleType: "none" }}>
+					<BoardPics board={board} />
+					{"   "}
+				</li>
+			))}
 			{boards.length === 0 && <div>"No boards found"</div>}
 		</ul>
 	);
