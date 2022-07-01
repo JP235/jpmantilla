1322000b4c2cf94a31c3d41c426a9611b473f841
jpmanti <jpmanti@gmail.com>
2022-07-01 16:45:43 -0500

    server url

--- src/api/urlconfig.js
+++ src/api/urlconfig.js
@@ -2,4 +2,4 @@ const LOCAL_URL = 'http://localhost:3000/';
 const SERVER_URL = 'https://klotski-api.herokuapp.com/'
 const CODESPACES_URL = 'https://jp235-klotski-backend-qxrw757xh9x95-8000.githubpreview.dev/'
 
-export const BASE_REQ_URL = LOCAL_URL;
+export const BASE_REQ_URL = SERVER_URL;
